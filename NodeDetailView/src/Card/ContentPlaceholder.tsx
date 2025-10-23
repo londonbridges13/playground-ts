223f1dd4bc35f00dcea694a5fff12d429dee7643
Lyndon McKay <lyndonmckay13@gmail.com>
2025-10-23 10:21:53 -0500

    fix: Update deprecated useInvertedScale to useDeprecatedInvertedScale
    - Fix framer-motion API change in ContentPlaceholder component
    - Resolves TypeScript build error on Heroku

--- NodeDetailView/src/Card/ContentPlaceholder.tsx
+++ NodeDetailView/src/Card/ContentPlaceholder.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import { LoremIpsum } from "react-lorem-ipsum";
-import { motion, useInvertedScale } from "framer-motion";
+import { motion, useDeprecatedInvertedScale } from "framer-motion";
 
 export const ContentPlaceholder = React.memo(() => {
-  const inverted = useInvertedScale();
+  const inverted = useDeprecatedInvertedScale();
   return (
     <motion.div
       className="content-container"
